b0d2795c39e47095fac35d2bc2aa434a653257c5
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-07 22:58:48 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,14 @@
 def dictionary
-  substitutes = {"hello" => "hi", "to, two, too" => "2", "for, four" => "4", "you" => "u", "at" => "@", "and" => "&"}
+  dictionary = {"hello" => "hi", "to, two, too" => "2", "for, four" => "4", "you" => "u", "at" => "@", "and" => "&"}
 end
 
 def word_substituter(string)
-  
+  string_array = string.split.map do |word|
+    if dictionary.key.include?(word)
+      word = dictionary[word]
+    else
+      word
+      end
+    end
+    puts string_array.join(" ")
+end
