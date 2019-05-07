aced0cfbf51e189797c6119c274ff2b234dc0609
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-07 22:59:30 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,13 +2,13 @@ def dictionary
   dictionary = {"hello" => "hi", "to, two, too" => "2", "for, four" => "4", "you" => "u", "at" => "@", "and" => "&"}
 end
 
-def word_substituter(string)
-  string_array = string.split.map do |word|
+def word_substituter(tweet)
+  tweet_array = tweet.split.map do |word|
     if dictionary.key.include?(word)
       word = dictionary[word]
     else
       word
       end
     end
-    puts string_array.join(" ")
+    puts tweet_array.join(" ")
 end
